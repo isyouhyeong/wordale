f1691538b7bc04150bdf78cef8dd5e2f4eef019e
aiden_Lee <isyouh77@gmail.com>
2024-05-27 10:33:06 +0900

    {워들} 최종수정
    하아 어렵다.

--- js/index.js
+++ js/index.js
@@ -2,19 +2,37 @@ const 정답 = "APPLE";
 
 let attempts = 0;
 let index = 0;
+let timer;
 
 function appStart() {
   const displayGameover = () => {
     const div = document.createElement("div");
     div.innerText = "게임이 종료됐습니다.";
-    div.style =
-      "display:flex; justify-content:center; align-items:center; position:absolute; top:40vh; left:38vw; background-color: white; width:200px; height: 100px;";
+    div.style.cssText = `
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: fixed;
+      top: 40%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: white;
+      color: black;
+      width: 200px;
+      height: 100px;
+      z-index: 9999;
+      border: 2px solid black;
+      text-align: center;
+    `;
     document.body.appendChild(div);
+    console.log("게임 종료 메시지 표시됨");
   };
 
   const gameover = () => {
     window.removeEventListener("keydown", handleKeydown);
     displayGameover();
+    clearInterval(timer);
+    console.log("게임 종료 함수 호출됨");
   };
 
   const nextLine = () => {
@@ -25,15 +43,16 @@ function appStart() {
 
   const handleEnterKey = () => {
     let 맞은_갯수 = 0;
-    //정답 확인
     for (let i = 0; i < 5; i++) {
       const block = document.querySelector(
         `.board-column[data-index='${attempts}${i}']`
       );
       const 입력한_글자 = block.innerText;
       const 정답_글자 = 정답[i];
-      if (입력한_글자 === 정답_글자) block.style.background = "#6aaa64";
-      else if (정답.includes(입력한_글자)) block.style.background = "#c9b458";
+      if (입력한_글자 === 정답_글자) {
+        맞은_갯수 += 1;
+        block.style.background = "#6aaa64";
+      } else if (정답.includes(입력한_글자)) block.style.background = "#c9b458";
       else block.style.background = "#787c7e";
       block.style.color = "white";
     }
@@ -41,8 +60,13 @@ function appStart() {
     else nextLine();
   };
 
-  const handleBackspace = (thisBlock) => {
-    thisBlock.innerText = "";
+  const handleBackspace = () => {
+    if (index > 0) {
+      const preBlock = document.querySelector(
+        `.board-column[data-index='${attempts}${index - 1}']`
+      );
+      preBlock.innerText = "";
+    }
     if (index !== 0) index -= 1;
   };
 
@@ -52,7 +76,7 @@ function appStart() {
     const thisBlock = document.querySelector(
       `.board-column[data-index='${attempts}${index}']`
     );
-    if (event.key === "Backspace") handleBackspace(thisBlock);
+    if (event.key === "Backspace") handleBackspace();
     else if (index === 5) {
       if (event.key === "Enter") handleEnterKey();
       else return;
@@ -62,6 +86,22 @@ function appStart() {
     }
   };
 
+  const startTimer = () => {
+    const 시작_시간 = new Date();
+
+    function setTime() {
+      const 현재_시간 = new Date();
+      const 흐른_시간 = new Date(현재_시간 - 시작_시간);
+      const 분 = 흐른_시간.getMinutes().toString().padStart(2, "0");
+      const 초 = 흐른_시간.getSeconds().toString().padStart(2, "0");
+      const timeDiv = document.querySelector("#timer");
+      timeDiv.innerText = `${분}:${초}`;
+    }
+    timer = setInterval(setTime, 1000);
+  };
+
+  startTimer();
   window.addEventListener("keydown", handleKeydown);
 }
+
 appStart();
